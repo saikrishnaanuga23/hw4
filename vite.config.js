efe4bb256f5745f10d580a9f65b0a94095c47131
saikrishnaanuga23 <146044657+saikrishnaanuga23@users.noreply.github.com>
2024-04-20 13:56:32 -0500

    changes made

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hw4/"
+  base: "/hw4/",
+  server: {
+    host: true
+  }
 })
